ee52e8fd9ef3220a03f51f90e5dc2778594ce8a5
unknown <thuranszkyzso@gmail.com>
2023-02-27 17:40:54 +0100

    add files

---
+++ functions/api.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const serverless = require('serverless-http')
+const app=express()
+const router = express.Router()
+
+
+router.get("/", (req, res)=>{
+  res.send("ok")
+})
+
+app.use("/", router)
+module.exports.handler= serverless(app)
